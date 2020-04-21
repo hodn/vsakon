d54d6553eecd3b100a93446305b6b1f3205d4575
Doan, Hoang <doanhoan@fbmi.cvut.cz>
2020-04-21 18:30:33 +0200

    colors, icon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import 'typeface-roboto';
 import { StylesProvider } from "@material-ui/styles";
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from "@material-ui/styles";
 import './App.css';
 import TopBar from './components/TopBar';
 import '../node_modules/react-vis/dist/style.css';
 import 'leaflet/dist/leaflet.css';
+import colors from './colors'
 const { ipcRenderer } = window.require('electron');
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: colors.secondary
+    },
+    secondary: {
+      main: colors.main}
+      
+  }
+})
 
 class App extends Component {
 
@@ -16,9 +30,11 @@ class App extends Component {
 
     return (
 
+      <ThemeProvider theme={theme}>
         <StylesProvider injectFirst>
-          <TopBar/>
+          <TopBar />
         </StylesProvider>
+      </ThemeProvider>
     );
   }
 }
