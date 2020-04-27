08f67e556343dfb916c85223e81e67fd084c3b9b
User <user@User-Mac-mini.local>
2020-04-27 21:35:49 +0200

    update electron, serialport

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -23,9 +23,9 @@ function createWindow() {
     //mainWindow.setFullScreen(true);
     mainWindow.maximize();
     // and load the index.html of the app.
-    mainWindow.loadURL(`file://${path.join(__dirname, '../build/index.html')}`);
+    //mainWindow.loadURL(`file://${path.join(__dirname, '../build/index.html')}`);
     mainWindow.setMenuBarVisibility(false)
-    //mainWindow.loadURL('http://localhost:3000');
+    mainWindow.loadURL('http://localhost:3000');
 
     // Open the DevTools.
     //mainWindow.webContents.openDevTools();
@@ -94,7 +94,7 @@ ipcMain.on('clear-to-send', (event, arg) => {
 
             ports.forEach(port => {
                 if (port.manufacturer === "FTDI") {
-                    flexiGuardPorts.push(port.comName);
+                    flexiGuardPorts.push(port.path);
                 }
             })
 
