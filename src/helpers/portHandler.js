c32c67fb195eb3068cae3c8c1c66af544cffafc1
Doan, Hoang <doanhoan@fbmi.cvut.cz>
2020-04-27 13:46:35 +0200

    artificial reading to detect close

--- src/helpers/portHandler.js
+++ src/helpers/portHandler.js
@@ -46,10 +46,13 @@ module.exports = class PortHandler {
 
         open();
 
+        const helperPort = this.port;
+
         // If open - inform Renderer
         this.port.on('open', function () {
             console.log("Connected " + com); 
             let portState = {port: com, state: "opened"};
+            helperPort.read();
             event.sender.send('port-state', portState); // Send the state to Renderer
         });
 
